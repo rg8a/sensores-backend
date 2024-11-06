f78d75b6e5d42c7da64270387ec0b7d1a689c7a7
Ochoagram <ricardogochoa@me.com>
2024-11-06 00:14:50 -0600

    init

---
+++ index.js
@@ -0,0 +1,53 @@
+require('dotenv').config();
+const express = require('express');
+const mysql = require('mysql2');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true })); // Añadir esto
+app.use(express.json()); // Parsear JSON
+
+// Configuración de la conexión a la base de datos
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+});
+
+// Conectar a la base de datos
+db.connect((err) => {
+  if (err) {
+    console.error('Error de conexión a la base de datos:', err);
+    return;
+  }
+  console.log('Conectado a la base de datos');
+});
+
+// Ruta de prueba en la raíz "/"
+app.get('/', (req, res) => {
+  res.send('Servidor funcionando');
+});
+
+// Endpoint para registrar los datos del sensor
+app.post('/sensor_datos', (req, res) => {
+  const { ldr, modo_operacion } = req.body;
+
+  // Query para insertar los datos en la tabla correspondiente
+  const query = `INSERT INTO registros_sensor6 (valor_fotoresistor, tiempo_registro, modo_operacion) VALUES (?, NOW(), ?)`;
+  db.query(query, [ldr, modo_operacion], (err, results) => {
+    if (err) {
+      console.error('Error al insertar los datos:', err);
+      res.status(500).json({ error: 'Error al insertar los datos' });
+    } else {
+      res.status(200).json({ message: 'Datos insertados exitosamente' });
+    }
+  });
+});
+
+// Iniciar el servidor
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
